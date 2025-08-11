b6717eb2251e8b8b60765245c59a1d43153cfaf4
Sayuri Bose <sayuribose@gmail.com>
2025-08-11 10:00:57 -0400

    change 2

--- GitHubCollab/ContentView.swift
+++ GitHubCollab/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, group8")
+            Text("Change 2")
         }
         .padding()
     }
