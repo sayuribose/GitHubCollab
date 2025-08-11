60b257dfb1547e12ec0819de90ad2c7307766bca
Sayuri Bose <sayuribose@gmail.com>
2025-08-11 09:47:47 -0400

    Initial Commit

---
+++ GitHubCollab/GitHubCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollabApp.swift
+//  GitHubCollab
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
