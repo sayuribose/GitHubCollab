dfc7457bcc8302ec2a9aa35446cc869442916905
Sayuri Bose <sayuribose@gmail.com>
2025-08-12 11:34:02 -0400

    changed the buttons design and text

--- GitHubCollab/ContentView.swift
+++ GitHubCollab/ContentView.swift
@@ -10,51 +10,76 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           
-            Text("MyMind")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Button {
-            } label: {
-                Text("Panic Attack")
-                    .padding(20)
-                    .background(Color.pink)
-                    .font(.title)
-                    .cornerRadius(95)
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            ZStack{
+                Image("daisy")
+                VStack{
+                    Text("MyMind")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Button {
+                    } label: {
+                        Text("Panic Attack")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .padding(.horizontal, 50.0)
+                            .padding(.top, 20.0)
+                            .padding(.bottom, 20.0)
+                            .background(Color.pink)
+                            .font(.title)
+                            .cornerRadius(95)
+                            
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .padding()
+                    Button {
+                    } label: {
+                        Text("Anxiety Attack")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .padding(.horizontal, 50.0)
+                            .padding(.top, 20.0)
+                            .padding(.bottom, 20.0)
+                            .background(Color.green)
+                            .font(.title)
+                            .cornerRadius(95)
+                            
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .padding()
+                    Button {
+                    } label: {
+                        Text("Overstimulation")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .padding(.horizontal, 50.0)
+                            .padding(.top, 20.0)
+                            .padding(.bottom, 20.0)
+                            .background(Color.teal)
+                            .font(.title)
+                            .cornerRadius(95)
+                            
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .padding()
+                    Button {
+                    } label: {
+                        Text("Distractedness")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .padding(.horizontal, 50.0)
+                            .padding(.top, 20.0)
+                            .padding(.bottom, 20.0)
+                            .background(Color.purple)
+                            .font(.title)
+                            .cornerRadius(95)
+                            
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .padding()
+                }
             }
-            Button {
-            } label: {
-                Text("Anxiety Attack")
-                    .padding(20)
-                    .background(Color.green)
-                    .font(.title)
-                    .cornerRadius(95)
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            Button {
-            } label: {
-                Text("Overstimulation")
-                    .padding(20)
-                    .background(Color.teal)
-                    .font(.title)
-                    .cornerRadius(95)
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            Button {
-            } label: {
-                Text("Distractedness")
-                    .padding(20)
-                    .background(Color.purple)
-                    .font(.title)
-                    .cornerRadius(95)
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            
-            
             
         }
-        
     }
 }
 
