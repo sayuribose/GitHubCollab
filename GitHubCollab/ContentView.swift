b8270d4eebb7cf848f0e7a423471771b70ba54ce
Sayuri Bose <sayuribose@gmail.com>
2025-08-12 11:20:40 -0400

    added title and buttons. changed design

--- GitHubCollab/ContentView.swift
+++ GitHubCollab/ContentView.swift
@@ -10,16 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, group8")
-            Text("Change 2")
-            Text("Change 3")
-            Text("Change 4")
-            Text("change-sayuri")
+           
+            Text("MyMind")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Button {
+            } label: {
+                Text("Panic Attack")
+                    .padding(20)
+                    .background(Color.pink)
+                    .font(.title)
+                    .cornerRadius(95)
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Button {
+            } label: {
+                Text("Anxiety Attack")
+                    .padding(20)
+                    .background(Color.green)
+                    .font(.title)
+                    .cornerRadius(95)
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Button {
+            } label: {
+                Text("Overstimulation")
+                    .padding(20)
+                    .background(Color.teal)
+                    .font(.title)
+                    .cornerRadius(95)
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Button {
+            } label: {
+                Text("Distractedness")
+                    .padding(20)
+                    .background(Color.purple)
+                    .font(.title)
+                    .cornerRadius(95)
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+            
+            
         }
-        .padding()
+        
     }
 }
 
