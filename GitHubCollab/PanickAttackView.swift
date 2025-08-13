f2df95671e6cd635f8f1cf673cd6c9ed246231a6
Sayuri Bose <sayuribose@gmail.com>
2025-08-13 10:44:33 -0400

    changed errors on panic attack file

--- GitHubCollab/PanickAttackView.swift
+++ GitHubCollab/PanickAttackView.swift
@@ -1,5 +1,5 @@
 //
-//  PanickAttackView.swift
+//  OverstimView.swift
 //  GitHubCollab
 //
 //  Created by Scholar on 8/12/25.
@@ -9,58 +9,28 @@ import SwiftUI
 
 struct PanickAttackView: View {
     var body: some View {
-       
-            
-            VStack{
+        VStack{
+            HStack {
+                Spacer()
                 
-<<<<<<< HEAD
-                HStack {
-                    Spacer()
-                    
-                    Text("Panic Attack")
-                        .foregroundStyle(.white)
-                        .fontWeight(.heavy)
-                        .font(.system(size: 55))
-                    
-                    Spacer()
-                    
-                }
-                
-=======
                 Text("Panic Attack")
                     .foregroundStyle(.white)
                     .fontWeight(.heavy)
                     .font(.system(size: 45))
->>>>>>> main
                 
-                .padding(.vertical, 10)
-                .padding()
-                .background(Color(hue: 0.933, saturation: 0.214, brightness: 0.971))
                 Spacer()
-<<<<<<< HEAD
-                ScrollView{
-                    //Text("hi")
-            }
-            
-            
-        }
-    }
-}
-=======
                 
             }
             .padding(.vertical, 10)
             .padding()
-            .background(Color(hue: 0.891, saturation: 0.24, brightness: 1.0))
+            .background(Color(hue: 0.933, saturation: 0.214, brightness: 0.971))
             Spacer()
+            ScrollView{
+                //Text("hi")
+            }
         }
-        }
-    
     }
-        
-
->>>>>>> main
-
+}
 #Preview {
     PanickAttackView()
 }
